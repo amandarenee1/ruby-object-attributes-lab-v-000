c0c493bb16e4c8bcb96df389a114cb26e762ddfb
Amanda Wingate <arw0219@gmail.com>
2018-01-28 22:00:43 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,9 +8,21 @@ class Dog
   def name
     "Fido"
   end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    "Beagle"
 end
 
 fido = Dog.new
 fido.name = "Fido"
 
 puts fido.name
+
+#Create class that reads and writes dog breed.
+
+snoopy = Dog.new
+snoopy.breed = "Beagle"
